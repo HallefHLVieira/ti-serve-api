6706ad28c869e5d3da5cbb99c59dc8f5619e1d32
Hallef Vieira <hallef.vieira.dev@gmail.com>
2024-06-28 23:52:14 -0300

    configure zod and create env files

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev','test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false ){
+  console.error('❌️ Invalid environment variables', _env.error.format());
+  
+  // Caso alguma env dê ruim, a aplicação deve cair
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
