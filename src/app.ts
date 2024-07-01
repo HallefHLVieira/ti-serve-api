594e364f6173fa354d03a4cbf66fd60ad6ade0b2
Hallef Vieira <hallef.vieira@hotmail.com>
2024-06-30 22:54:39 -0300

    create post route to test insert

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,29 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6),
+    phone: z.string().min(11),
+  })
+
+  const { name, email, password, phone } = registerBodySchema.parse(
+    request.body,
+  )
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      phone,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
