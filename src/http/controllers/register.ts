87ace02ccc76e502ad4ac3571c916f595d99d1ec
Hallef Vieira <hallef.vieira.dev@gmail.com>
2024-07-01 08:21:09 -0300

    implement bcrypt to password user

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { prisma } from '../../lib/prisma'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,12 +15,24 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     request.body,
   )
 
+  const password_hash = await hash(password, 5)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send('User already exist!')
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
       phone,
-      password_hash: password,
+      password_hash,
     },
   })
 
