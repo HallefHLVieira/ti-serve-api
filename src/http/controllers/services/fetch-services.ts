019818747fbb2598d7132d3173bd31314897b5df
Hallef Vieira <hallef.vieira@hotmail.com>
2024-09-06 10:26:56 -0300

    chore: update services controller

--- src/http/controllers/services/fetch-services.ts
+++ src/http/controllers/services/fetch-services.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { makeFetchServicesUseCase } from '@/use-cases/factories/make-fetch-services-use-case'
+import { makeFetchFollowersByServiceUseCase } from '@/use-cases/factories/make-fetch-followers-by-service-use-case'
 
 export async function fetchServicesController(
   request: FastifyRequest,
@@ -7,10 +8,26 @@ export async function fetchServicesController(
 ) {
   try {
     const fetchServicesUseCase = makeFetchServicesUseCase()
+    const fetchFollowersByServiceUseCase = makeFetchFollowersByServiceUseCase()
 
-    const services = await fetchServicesUseCase.execute()
+    const { services } = await fetchServicesUseCase.execute()
+
+    const updatedServices = await Promise.all(
+      services.map(async (service) => {
+        const { followers } = await fetchFollowersByServiceUseCase.execute({
+          serviceId: service.id,
+        })
+
+        const likes = {
+          likes: followers.length,
+        }
+
+        return Object.assign(service, likes)
+      }),
+    )
+
     return reply.status(200).send({
-      services,
+      updatedServices,
     })
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
