ee20580655824f6c9a856b398cc98d39b8588c11
Hallef Vieira <hallef.vieira@hotmail.com>
2024-09-13 13:57:34 -0300

    chore: implement prop phones in get service by id

--- src/http/controllers/services/fetch-service-by-id.ts
+++ src/http/controllers/services/fetch-service-by-id.ts
@@ -1,3 +1,4 @@
+import { makeFetchFollowersByServiceUseCase } from '@/use-cases/factories/make-fetch-followers-by-service-use-case'
 import { makeFetchPhonesByServiceUseCase } from '@/use-cases/factories/make-fetch-phones-by-service-use-case'
 import { makeGetServiceByIdUseCase } from '@/use-cases/factories/make-get-service-by-id-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
@@ -19,9 +20,14 @@ export async function fetchServicesByIdController(
     })
 
     const fetchPhonesByServiceUseCase = makeFetchPhonesByServiceUseCase()
-    const phones = await fetchPhonesByServiceUseCase.execute({ serviceId })
+    const { phones } = await fetchPhonesByServiceUseCase.execute({ serviceId })
 
-    return reply.status(200).send({ ...service, phones })
+    const fetchLikesByService = makeFetchFollowersByServiceUseCase()
+    const { followers } = await fetchLikesByService.execute({ serviceId })
+
+    return reply
+      .status(200)
+      .send({ ...service, phones, likes: followers.length })
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     return reply.status(500).send({ message: err.message })
