2640b9af74dea95033c38c59f011ada1974dcd23
Hallef Vieira <hallef.vieira@hotmail.com>
2024-10-20 22:50:37 -0300

    chore: remove not used code

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import fastify from 'fastify'
 import { usersRoutes } from './http/controllers/users/routes'
-import { ZodError, z } from 'zod'
+import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
 import { servicesRoutes } from './http/controllers/services/routes'
 import { followersRoutes } from './http/controllers/follwers/routes'
-import { r2 } from './lib/cloudfare'
-import { PutObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { randomUUID } from 'crypto'
 
 export const app = fastify()
 
@@ -34,26 +30,6 @@ app.register(
   },
   { prefix: '/catalog-api' },
 )
-
-app.post('/uploads', async (request) => {
-  const uploadBodySchema = z.object({
-    name: z.string().min(1),
-    contentType: z.string().regex(/\w+\/[-+.\w]+/),
-  })
-  const { name, contentType } = uploadBodySchema.parse(request.body)
-  const filekey = randomUUID().concat('-').concat(name)
-
-  const signedUrl = await getSignedUrl(
-    r2,
-    new PutObjectCommand({
-      Bucket: 'tiserve-dev',
-      Key: filekey,
-      ContentType: contentType,
-    }),
-    { expiresIn: 600 },
-  )
-  return signedUrl
-})
 
 app.get('/catalog-api/livenesscheck', () => ({
   service: 'services-catalog-api',
